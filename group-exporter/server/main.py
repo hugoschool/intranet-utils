51be9d649b8a163bb79b8c9f833b2dc609154505
Hugo ARNAL <hugo@hugoarnal.com>
2026-04-18 19:32:10 +0200

    group-exporter: add CORS middleware

--- group-exporter/server/main.py
+++ group-exporter/server/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI, Response, status
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import json
 
 app = FastAPI()
+
+origins = [
+    "http://intra.epitech.eu",
+    "https://intra.epitech.eu",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 JSON_FILE = ".groups.json"
 
