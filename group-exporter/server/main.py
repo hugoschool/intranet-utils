852ce05f46f01ec5c3a476ee76f43222b69c2e46
Hugo ARNAL <hugo@hugoarnal.com>
2026-04-20 23:28:00 +0200

    group-exporter: visualizer

--- group-exporter/server/main.py
+++ group-exporter/server/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, Response, status, File, UploadFile
+from fastapi import FastAPI, Request, Response, status, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from pathlib import Path
 from constants import IMAGES_FOLDER, JSON_FILE
@@ -7,6 +10,9 @@ import os
 import json
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 origins = [
     "http://intra.epitech.eu",
@@ -64,6 +70,18 @@ def group_page(group: GroupPage, response: Response):
 def groups():
     return json.loads(get_json_file_contents())
 
+@app.get("/image/{login}")
+def get_image(login: str):
+    filename = f"{IMAGES_FOLDER}/" + login + ".jpg"
+
+    if not os.path.isfile(filename):
+        return Response(status_code=404)
+
+    with open(filename, "rb") as f:
+        content = f.read()
+
+    return Response(content, media_type="image/jpeg")
+
 @app.post("/image")
 def upload_image(file: UploadFile, response: Response):
     Path(IMAGES_FOLDER).mkdir(parents=True, exist_ok=True)
@@ -76,8 +94,32 @@ def upload_image(file: UploadFile, response: Response):
         f.write(file.file.read())
         return {"status": "Success"}
 
-@app.get("/images")
-def list_images():
-    if not os.path.exists(IMAGES_FOLDER):
-        return []
-    return os.listdir(IMAGES_FOLDER)
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    content = json.loads(get_json_file_contents())
+    return templates.TemplateResponse(
+        request=request, name="visualizer-root.html", context={
+            "modules": list(set(content))
+        }
+    )
+
+@app.get("/{module}", response_class=HTMLResponse)
+def module_root(request: Request, module: str):
+    content = json.loads(get_json_file_contents())
+    return templates.TemplateResponse(
+        request=request, name="visualizer-module.html", context={
+            "module": module,
+            "projects": list(set(content[module]))
+        }
+    )
+
+@app.get("/{module}/{project}", response_class=HTMLResponse)
+def project_root(request: Request, module: str, project: str):
+    content = json.loads(get_json_file_contents())
+    return templates.TemplateResponse(
+        request=request, name="visualizer.html", context={
+            "module": module,
+            "project": project,
+            "groups": content[module][project]
+        }
+    )
