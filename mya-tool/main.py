4743716a222d4e4fb49ab2fc96863191f9006f0e
Hugo ARNAL <hugo@hugoarnal.com>
2026-08-03 18:36:05 +0200

    mya-tool: destinations subcommand

--- mya-tool/main.py
+++ mya-tool/main.py
@@ -51,21 +51,109 @@ def download_programs(args: Namespace) -> None:
         print(f"Successfully wrote to {args.output}")
 
 
-def get_all_destinations(args: Namespace) -> None:
-    with open(args.input, "r") as f:
-        data = json.loads(f.read())
+def load_programs(input_file: str):
+    with open(input_file, "r") as f:
+        return json.loads(f.read())
 
-        destinations: set[str] = set()
 
-        for program in data:
+class Destinations:
+    def __init__(self, args: Namespace):
+        self.args = args
+        self.content = load_programs(args.input)
+
+        subcommands = {"country": self.list_all_countries, "school": self.school}
+
+        subcommands[args.destinations_subparser_name]()
+
+    @staticmethod
+    def arguments(args):
+        _ = args.add_argument(
+            "-i",
+            "--input",
+            help="Input file",
+            type=str,
+            default="programs.json",
+            required=False,
+        )
+
+        subparsers = args.add_subparsers(dest="destinations_subparser_name")
+        country_args = subparsers.add_parser("country")  # noqa: F841
+
+        school_args = subparsers.add_parser("school")
+
+        _ = school_args.add_argument(
+            "--name",
+            help="List only names",
+            action="store_true",
+            default=False,
+            required=False,
+        )
+        _ = school_args.add_argument(
+            "--price",
+            help="List only prices",
+            action="store_true",
+            default=False,
+            required=False,
+        )
+        _ = school_args.add_argument(
+            "--country",
+            help="Name of the country",
+            type=str,
+            default="",
+            required=False,
+        )
+
+    def verify_country(self):
+        if "country" not in self.args or self.args.country == "":
+            raise ValueError("Missing destination country")
+
+    @staticmethod
+    def replace_country_aliases(country: str) -> str:
+        aliases = {"UK": "United Kingdom", "USA": "United States of America"}
+
+        if country in aliases:
+            return aliases[country]
+        return country
+
+    @staticmethod
+    def correct_price(price: float) -> float:
+        return price / 100
+
+    def list_all_countries(self) -> None:
+        countries: set[str] = set()
+
+        for program in self.content:
             for location in program["locations"]:
-                destinations.add(location["label"])
+                countries.add(location["label"])
 
-        destinations = list(destinations)
-        destinations.sort()
+        countries = list(countries)
+        countries.sort()
 
-        for destination in destinations:
-            print(destination)
+        for country in countries:
+            print(country)
+
+    def school(self) -> None:
+        self.verify_country()
+        country = self.replace_country_aliases(self.args.country)
+
+        schools = []
+
+        for program in self.content:
+            locations = set()
+
+            for location in program["locations"]:
+                locations.add(location["label"])
+
+            if country in locations:
+                schools.append(program)
+
+        for school in schools:
+            if self.args.name:
+                print(school["name"])
+            elif self.args.price:
+                print(self.correct_price(school["priceCents"]))
+            else:
+                print(f"{school['name']},{self.correct_price(school['priceCents'])}")
 
 
 def main() -> None:
@@ -82,16 +170,8 @@ def main() -> None:
         required=False,
     )
 
-    get_all_destinations_args = subparsers.add_parser("get-all-destinations")
-    _ = get_all_destinations_args.add_argument(
-        "-i",
-        "--input",
-        help="Input file",
-        type=str,
-        default="programs.json",
-        required=False,
-    )
-
+    destinations_args = subparsers.add_parser("destinations")
+    Destinations.arguments(destinations_args)
     args = args.parse_args()
 
     if args.subparser_name is None:
@@ -99,7 +179,7 @@ def main() -> None:
 
     commands = {
         "download": download_programs,
-        "get-all-destinations": get_all_destinations,
+        "destinations": Destinations,
     }
 
     commands[args.subparser_name](args)
